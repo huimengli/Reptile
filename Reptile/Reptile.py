02c3cd44dff3b73ff12c25192f492c1fa5b9af5a
绘梦璃 <2913394404@qq.com>
2023-07-13 12:38:26 +0800

    爬书

--- Reptile/Reptile.py
+++ Reptile/Reptile.py
@@ -3,17 +3,19 @@ import re
 import os
 import time
 import random
+import math
 
-webUrl = "http://www.aojieshengyuan.com/book/21472/";
-webUrlForEach = "http://www.aojieshengyuan.com/book/21472/";
+webUrl = "https://www.ibiquges.com/14/14795/";
+webUrlForEach = "https://www.ibiquges.com";
 file = "output.txt";
 ini = "ouput.ini";
-start = 11                                  #初始推荐章节数量
+start = 10                                  #初始推荐章节数量
 passUrl = '/html/13/13722/7099871.shtml'    #排除的对象(URL排除)
 passName = "无标题章节";                    #排除的对象(章节名排除)
 needProxy = False;                          #下载网站是否需要代理
 needVerify = True;                         #是否需要网页ssl证书验证
 ignoreDecode = False;                        #忽略解码错误内容
+timeWait = [2,4];                           #等待时间([最小值,最大值])
 
 #----------------------------------------------------------#
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36' }
@@ -21,7 +23,8 @@ headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36
 #readDD = re.compile(r'<[dd|li]{2} class="col-4">[\t\0\ \n]*<a href="([^"<>]*)"[^<>]*>([^<>]*)<\/a>');
 #readDD = re.compile(r'<[dd|li]{2}>[\t\0\ \n]*<[Aa] ?(alt=[^<>]*)? href=["\']([^"\'<>]*)[\'"][^<>]*>([^<>]*)(<!>)?<\/[Aa]>');
 #readDD = re.compile(r'<[dd|li]{2}>[\t\0\ \n]*<[Aa] ?(alt|title=[^<>]*)? href ?=["\']([^"\'<>]*)[\'"][^<>]*>([^<>]*)<\/[Aa]>');
-readDD = re.compile(r'<[dd|li]{2} class="book-item">[\t\0\ \n]*<[Aa] ?(style=[^<>]*)? href ?=["\']([^"\'<>]*)[\'"][^<>]*>([^<>]*)<\/[Aa]>');
+#readDD = re.compile(r'<[dd|li]{2} class="book-item">[\t\0\ \n]*<[Aa] ?(style=[^<>]*)? href ?=["\']([^"\'<>]*)[\'"][^<>]*>([^<>]*)<\/[Aa]>');
+readDD = re.compile(r'<[dd|li]{2}>[\t\0\ \n]*<[Aa] ?(style=[^<>]*)? href ?=["\']([^"\'<>]*)[\'"][^<>]*>([^<>]*)<\/[Aa]>');
 #readDD = re.compile(r'<[dd|li]{2}>[\t\0\ \n]*<[Aa] ?(style|alt|title=[^<>]*)? href ?=["\']([^"\'<>]*)[\'"][^<>]*>([^<>]*)<\/[Aa]>');
 #readDD = re.compile(r'<[Aa] ?(alt|title=[^<>]*)? href ?=["\']([^"\'<>]*)[\'"][^<>]*>([^<>]*)<\/[Aa]>');
 r = random.Random();
@@ -92,6 +95,22 @@ def changeIniIndex(index:int):
     lines[3] = "INDEX:"+str(index);
     openWtite(ini,"".join(lines));
     return;
+
+def getTime(second:int):
+    '''
+    根据秒计算时间
+    '''
+    s = second % 60
+    second = second // 60
+    m = second % 60
+    second = second // 60
+    h = second % 24
+    second = second // 24
+    ret = second and str(second) + "天" or ""
+    ret+= h and str(h) + "小时" or ""
+    ret += m and str(m) + "分钟" or ""
+    ret += str(s) + "秒"
+    return ret;
 
 try:
     # 实例化产生请求对象
@@ -194,6 +213,7 @@ try:
     urladds = urladds[i:]             #跳过已有章节
     names = names[i:]             #跳过已有章节
     need = len(urladds);
+    pageCount = len(allDD);         #总章节数量
     
     for j in range(0,need):
         
@@ -250,13 +270,16 @@ try:
         #text = re.compile(r'<div id="chaptercontent"[^<>]*>([\s\S]*)'+webUrlForEach)
         #text = re.compile(r'div id="content">([\s\S]*)<\/div>\n<a')
         #text = re.compile(r'div id="content">([\s\S]*)<\/div>[\r\n]*<a')
-        text = re.compile(r'<div class="posterror">([\s\S]*)[\r\n]*<a href="javascript:;" on')
+        #text = re.compile(r'<div class="posterror">([\s\S]*)[\r\n]*<a href="javascript:;" on')
+        #text = re.compile(r'<div id="content" deep="3">([\s\S]*)[\r\n]*<a href="javascript:;" on')
+        #text = re.compile(r'<div id="content" deep="3">([\s\S]*)[\r\n]*<br>网页版章节内容慢')
         #text = re.compile(r'div id="content">([\s\S]*)<\/div>[\r\n\t\ ]*<div class="bottem2">')
         #text = re.compile(r'div id="content">([\s\S]*)<\/div>[\r\n\t\ ]*<div class="readerFooterNav"')
         #text = re.compile(r'div id="content">([\s\S]*)<br /><br /><p>')
         #text = re.compile(r'<div class="content" id="chaptercontent">([\s\S]*)<div class="info bottominfo">')
         #text = re.compile(r'<div id="content" name="content">([\s\S]*)<center class="clear">')
         #text = re.compile(r'div id="content" class="showtxt">([\s\S]*)<\/div>\n<script>read3')
+        text = re.compile(r'div id="content">([\s\S]*)<\/div>[\n\t\0\r\ ]*<script>read3')
         #text = re.compile(r'div id="content" class="showtxt">([\s\S]*)<script')
         #text = re.compile(r'div id="content" class="showtxt">([\s\S]*)<script')
         #text = re.compile(r'div id="content" class="showtxt">([\s\S]*)<div class="page_chapter">')
@@ -270,8 +293,10 @@ try:
         allText = allText.replace("&nbsp;"," ");
         allText = allText.replace("<br /><br />","\n");
         allText = allText.replace("<br/><br/>","\n");
+        allText = allText.replace("<br><br>","\n");
         allText = allText.replace("<br />","\n");
         allText = allText.replace("<br/>","\n");
+        allText = allText.replace("<br>","\n");
         allText = allText.replace("<p>","");
         allText = allText.replace("</p>","\n");
         allText = allText.replace("\n\n","\n");
@@ -288,12 +313,12 @@ try:
         #openWrites(allText[:len(allText)-3]);       #去掉最后行尾网站信息
         openWriteAdd(allText);                      #单行内容
         
-        print("第"+str(i)+"章已经下载完成");
+        print("\r","第"+str(i)+"章已经下载完成 进度: "+str(math.floor(i/pageCount*10000)/100)+"% ,ETA: "+getTime((pageCount-i)*(timeWait[0]+timeWait[1])//2),end="             ",flush=True);
         i+=1;
         changeIniIndex(i);
         #time.sleep(r.randint(3,7));             #有爬取限制的网站
         #time.sleep(r.randint(0,1));             #无爬取限制的网站
-        time.sleep(r.randint(1,4));             #无爬取限制的网站
+        time.sleep(r.randint(timeWait[0],timeWait[1]));             #无爬取限制的网站
 
 except Exception as e:
     #changeIniIndex(i);
