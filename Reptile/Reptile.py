5034654429321288520f4811e9541356bb0619de
zimingdehua <zimingdehua@163.com>
2023-08-18 12:41:03 +0800

    爬取大奉打更人

--- Reptile/Reptile.py
+++ Reptile/Reptile.py
@@ -5,18 +5,18 @@ import time
 import random
 import math
 
-webUrl = "http://www.cxbz958.org/xianzibienaole/?wkbatg=q54vx3";
-webUrlForEach = "http://www.cxbz958.org";
+webUrl = "http://www.shreducer.com/sd/13763/";
+webUrlForEach = "http:";
 file = "output.txt";
 ini = "ouput.ini";
-start = 10 +13                                #初始推荐章节数量
+start = 7                                #初始推荐章节数量
 passUrl = ''                                #排除的对象(URL排除)
 passName = "无标题章节";                    #排除的对象(章节名排除)
 needProxy = False;                          #下载网站是否需要代理
 needVerify = True;                         #是否需要网页ssl证书验证
 ignoreDecode = False;                        #忽略解码错误内容
 isLines = False;                             #内容是否是多行的
-haveTitle = False;                          #是否有数字章节头(为了小说阅读器辨别章节用)
+haveTitle = True;                          #是否有数字章节头(为了小说阅读器辨别章节用)
 timeWait = [4,6];                           #等待时间([最小值,最大值])
 maxErrorTimes = 50;                          #章节爬取最大错误次数
 
@@ -326,8 +326,9 @@ try:
             #text = re.compile(r'<div id="content" name="content">([\s\S]*)<center class="clear">')
             #text = re.compile(r'div id="content" class="showtxt">([\s\S]*)<\/div>\n<script>read3')
             #text = re.compile(r'div id="content">([\s\S]*)<\/div>[\n\t\0\r\ ]*<script>read3')
+            text = re.compile(r'div id="content">([\s\S]*)<\/div>[\n\t\0\r\ ]*<div class="adtxt">')
             #text = re.compile(r'div id="content" class="showtxt">([\s\S]*)<script')
-            text = re.compile(r'div id="content" class="showtxt">([\s\S]*)<script>read3')
+            #text = re.compile(r'div id="content" class="showtxt">([\s\S]*)<script>read3')
             #text = re.compile(r'div id="content" class="showtxt">([\s\S]*)<script>showByJs')
             #text = re.compile(r'div id="content" class="showtxt">([\s\S]*)<div class="page_chapter">')
             #text = re.compile(r'<script>read2\(\);</script>([\s\S]*)<script>app2\(\);</script>')
