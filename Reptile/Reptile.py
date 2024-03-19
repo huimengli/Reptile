3b470189978a776fc20165fdff96ec7c5118d2a3
听 楼 <2913394404@qq.com>
2024-03-19 21:41:06 +0800

    爬取<极品附马爷>

--- Reptile/Reptile.py
+++ Reptile/Reptile.py
@@ -5,18 +5,18 @@ import time
 import random
 import math
 
-webUrl = "https://www.zzxx.org/xs/26162/";
-webUrlForEach = "https://www.zzxx.org";
+webUrl = "https://www.kanshushi.com/shu/193127/";
+webUrlForEach = "https://www.kanshushi.com";
 file = "output.txt";
-ini = "ouput.ini";
-start = 10 + 0                              #初始推荐章节数量
+ini = "output.ini";
+start = 10 + 1                              #初始推荐章节数量
 passUrl = ''                                #排除的对象(URL排除)
 passName = "无标题章节";                    #排除的对象(章节名排除)
 needProxy = False;                          #下载网站是否需要代理
 needVerify = True;                         #是否需要网页ssl证书验证
 ignoreDecode = False;                        #忽略解码错误内容
 isLines = False;                             #内容是否是多行的
-haveTitle = False;                          #是否有数字章节头(为了小说阅读器辨别章节用)
+haveTitle = True;                          #是否有数字章节头(为了小说阅读器辨别章节用)
 timeWait = [1,3];                           #等待时间([最小值,最大值])
 maxErrorTimes = 10;                          #章节爬取最大错误次数
 removeHTML = False;                         #是否移除文章中的URL地址(测试功能)
@@ -508,6 +508,7 @@ try:
             #text = re.compile(r'div id="content" deep="3">([\s\S]*)<br><br>\n为您提供大神薪意')
             #text = re.compile(r'<div id="content" deep="3">([\s\S]*)[\r\n]*<a href="javascript:;" on')
             #text = re.compile(r'<div id="content" deep="3">([\s\S]*)[\r\n]*<br>网页版章节内容慢')
+            text = re.compile(r'<div id="content">([\s\S]*)[\r\n]*<br>网页版章节内容慢')
             #text = re.compile(r'<div id="content" deep="3">([\s\S]*)无尽的昏迷过后')
             #text = re.compile(r'div id="content">([\s\S]*)无尽的昏迷过后')
             #text = re.compile(r'div id="content">([\s\S]*)有的人死了，但没有完全死……')
@@ -519,7 +520,7 @@ try:
             #text = re.compile(r'<script>read2\(\);</script>([\s\S]*)<script>app2\(\);</script>')
             #text = re.compile(r'<script>app2\(\);</script>([\s\S]*)<script>app2\(\);</script>')
             #text = re.compile(r'<div id="chaptercontent" class="Readarea ReadAjax_content">([\s\S]*)<p class="readinline">')
-            text = re.compile(r'<div id="htmlContent">([\s\S]*)<div class="bottem">')
+            #text = re.compile(r'<div id="htmlContent">([\s\S]*)<div class="bottem">')
         else:
             text = re.compile(r'<p class=".*">([^<>]*)<\/p>')
             #text = re.compile(r'<p>([^<>]*)<\/p>')
