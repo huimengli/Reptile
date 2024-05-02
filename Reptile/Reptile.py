37b2e9333e583bc574a58d72fa82e1ad05b07f98
绘梦璃 <2913394404@qq.com>
2024-05-02 22:26:34 +0800

    爬书

--- Reptile/Reptile.py
+++ Reptile/Reptile.py
@@ -5,11 +5,10 @@ import time
 import random
 import math
 
-webUrl = "https://www.xswang.vip/book/65942/";
-webUrlForEach = "https://www.x23wxw6.com";
+webUrl = "https://www.bqgbi.cc/book/1466/";
 file = "output.txt";
 ini = "output.ini";
-start = 10 + 13                              #初始推荐章节数量
+start = 10 + -1                              #初始推荐章节数量
 passUrl = ''                                #排除的对象(URL排除)
 passName = "无标题章节";                    #排除的对象(章节名排除)
 needProxy = False;                          #下载网站是否需要代理
@@ -538,9 +537,10 @@ try:
             #text = re.compile(r'div id="content" class="showtxt">([\s\S]*)<div class="page_chapter">')
             #text = re.compile(r'div id="content" class="showtxt">([\s\S]*)<script>app2\(\);</script>')
             #text = re.compile(r'<script>read2\(\);</script>([\s\S]*)<script>app2\(\);</script>')
-            text = re.compile(r'<script>read2\(\);</script>([\s\S]*)<script>read3')
+            #text = re.compile(r'<script>read2\(\);</script>([\s\S]*)<script>read3')
             #text = re.compile(r'<script>app2\(\);</script>([\s\S]*)<script>app2\(\);</script>')
             #text = re.compile(r'<div id="chaptercontent" class="Readarea ReadAjax_content">([\s\S]*)<p class="readinline">')
+            text = re.compile(r'<div id="chaptercontent" class="Readarea ReadAjax_content">([\s\S]*)请收藏本站：http')
             #text = re.compile(r'<div id="htmlContent">([\s\S]*)<div class="bottem">')
         else:
             #text = re.compile(r'<p class=".*">([^<>]*)<\/p>')
