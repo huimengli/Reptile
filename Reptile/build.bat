26fb29793474735ca7229a4e9c60849cd2397bf1
huimengli <2913394404@qq.com>
2024-08-02 17:10:15 +0800

    修改了main.exe的构建脚本,修改了读取网站内容为无法解析时报错的问题

--- Reptile/build.bat
+++ Reptile/build.bat
@@ -1,5 +1,5 @@
 @echo off
-setlocal
+setlocal enabledelayedexpansion
 
 :: 设置 Python 脚本的名称
 set SCRIPT_NAME=main
@@ -13,8 +13,42 @@ set EXECUTABLE_NAME=%SCRIPT_NAME%.exe
 :: 切换到脚本所在的目录
 cd /d %~dp0
 
+:: 初始化计数器
+set count=0
+
+:: 查找以env或venv开头的文件夹并列出
+for /d %%i in (env* venv*) do (
+    set /a count+=1
+    echo !count!. %%i
+    set "env_!count!=%%i"
+)
+
+:: 如果没有找到符合条件的文件夹
+if %count% equ 0 (
+    echo No virtual environments found.
+    exit /b 1
+)
+
+:: 选择虚拟环境
+set /p choice=Choose your virtual environment (1-%count%, 0 to exit):
+
+:: 退出选项
+if "%choice%" equ "0" (
+    echo Exiting...
+    exit /b 0
+)
+
+:: 检查选择是否在有效范围内
+if %choice% leq 0 if %choice% gtr %count% (
+    echo Invalid choice.
+    exit /b 1
+)
+
+:: 设置选择的虚拟环境路径
+set VIRTUAL_ENV=!env_%choice%!
+
 :: 调用 PyInstaller 来打包 Python 脚本
-call env\Scripts\pyinstaller.exe --add-data "write.exe;." --onefile %SCRIPT_FULL_NAME%
+call %VIRTUAL_ENV%\Scripts\pyinstaller.exe --add-data "write.exe;." --onefile %SCRIPT_FULL_NAME%
 
 :: 检查 main.exe 是否生成
 if not exist dist\%EXECUTABLE_NAME% (
